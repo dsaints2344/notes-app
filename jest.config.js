90d825ce667106a9b66dc84354b8c5536839c9d1
David De Los Santos <ddelossantosmateo@gmail.com>
2020-01-17 03:21:46 -0400

    fsdf
    sdf

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   reporters: ['default', ['jest-junit', {
-    outputDirectory: 'reports',
+    outputDirectory: 'reports/junit',
   }]],
 };
